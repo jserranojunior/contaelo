19944443d6031bad57bc831caa0d86541d075cca
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2019-10-03 18:59:18 -0300

    cpf

--- database/migrations/2019_10_03_185255_cpf_null.php
+++ database/migrations/2019_10_03_185255_cpf_null.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyCpfUsers extends Migration
+class CpfNull extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class ModifyCpfUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('cpf');
-           
+            $table->string('cpf')->change();
         });
     }
 
